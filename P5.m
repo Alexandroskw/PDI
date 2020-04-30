a946a495222b09c725db4d999d366cd48a8a96db
Alexandroskw <Alexandros@kw.org>
2020-04-30 02:51:25 -0500

    Agregando 4x4

--- P5.m
+++ P5.m
@@ -2,7 +2,7 @@ clc; clear all; close all;
 
 im_data = imread("pentagon256x256.tif");
 
-subplot(1, 2, 1)
+figure(1)
 imshow(im_data);
 title("Original Image")
 
@@ -15,22 +15,49 @@ C = zeros((2*szImage(1)), (2*szImage(2)));
 pos = 1;
 cont = 0;
 
-subplot(1, 2, 2)
-
+figure(2)
 for i=1:szImage(1)
     pos = i+cont;
     B(pos,:) = im_data(i,:);
     cont = cont + (2-1);
 end
 
-szB = size(B);
+pos1 = 1;
+cont1 = 0;
+
+%szB = size(B);
 
 for i=1:szImage(2)
-    pos = i+cont;
-    C(:, pos) = B(:, i);
-    cont = cont+(2-1);
+    pos1 = i+cont1;
+    C(:, pos1) = B(:, i);
+    cont1 = cont1+(2-1);
 end
 
-imagesc(C);
+inter = uint8(conv2(C, [1 5], 'same'));
+dft = fft2(C);
+
 colormap ('gray');
+imagesc(log10(abs(fftshift(dft))));
 title("2x2")
+
+%% 4x4
+
+B1 = zeros(4*szImage(1), szImage(2));
+C1 = zeros((4*szImage(1)), (4*szImage(2)));
+
+p = 1;
+c = 0;
+
+figure(3)
+for i=1:szImage2(1)
+    p = i+c;
+    B1(p,:) = im_data(i,:);
+    c = c + (4-1);
+end
+
+p1 = 1;
+c1 = 0;
+
+for i=1:sZImage(2)
+    
+end
