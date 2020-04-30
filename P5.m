b1f0f1ef1c9e92b461947ca05b4a016cb5ef6f83
Alexandroskw <Alexandros@kw.org>
2020-04-29 19:56:38 -0500

    Cambios en practica 5

--- P5.m
+++ P5.m
@@ -1,31 +1,37 @@
+clc; clear all; close all;
+
 im_data = imread("pentagon256x256.tif");
 
 subplot(1, 2, 1)
 imshow(im_data);
 title("Original Image")
 
-matrix_2x2 = [0 0;0 0];
-matrix_4x4 = [0 0 0 0;0 0 0 0;0 0 0 0;0 0 0 0];
-
 szImage = size(im_data);
+szImage2 = size(im_data);
 
-B=zeros((2*szImage),szImage(2));
-C=zeros((2*szImage(1)),(2*szImage(2)));
+dsz = 2*szImage;
+dsz2 = 2*szImage2;
+
+B = zeros(dsz);
+C = zeros();
 
 pos = 1;
-c = 0;
+cont = 0;
 
-for i=1:szImage(1)
-    pos = i+c;
-    B(pos, :) = image(i, :);
-    c = c + (4-1);
+subplot(1, 2, 2)
+for i=1:szImage
+    pos = i+cont;
+    B(pos,:) = double(i);
+    cont = cont + (2-1);
+    ImF = fftshift(fft2(i));
 end
+Im = log(1.0 + ImF);
+imagesc(Im);
 
-szB = size(B);
+%szB = size(B);
 
-for i=1:szB(2)
-    pos = i+c;
-    C(:, pos) = B(:, i);
-    c = c+(4-1);
-end
-
+%for i=1:szB(2)
+%    pos = i+cont;
+%    C(:, pos) = B(:, i);
+%    cont = cont+(2-1);
+%end
